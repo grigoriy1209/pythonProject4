ee80dfe23333eb3435581b86a335ae1f735c32e3
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-10-30 19:29:37 +0200

    tests

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -2,7 +2,7 @@ import {authService} from "./authService";
 import {w3cwebsocket as W3cwebsocket} from 'websocket'
 
 
-const baseURL = 'ws://localhost/api'
+const baseURL = 'ws://testdrf.us-east-1.elasticbeanstalk.com/api'
 
 const socketService = async ()=> {
     const {data:{token}} = await authService.getSocketToken();
